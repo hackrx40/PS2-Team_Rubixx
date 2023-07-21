8f4bda6fbbba7be435f467fce94a9933afa81876
kartikey321 <kartikeymahawar1234@gmail.com>
2023-07-22 02:03:44 +0530

    added preferences and categories

--- frontend-app/lib/screens/preference_screen.dart
+++ frontend-app/lib/screens/preference_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mediserv/data/categories_list.dart';
+import 'package:mediserv/utils/colors.dart';
 
 class PreferenceScreen extends StatefulWidget {
   const PreferenceScreen({super.key});
@@ -11,49 +14,101 @@ class PreferenceScreen extends StatefulWidget {
 }
 
 class _PreferenceScreenState extends State<PreferenceScreen> {
- List categories= [{"title":"Aarogya Care","clicked":false,"image":""},{"title":"Ayurveda","clicked":false,"image":""},{"title":"Bone Health","clicked":false,"image":""},{"title":"Cancer","clicked":false,"image":""},{"title":"Child Care","clicked":false,"image":""},{"title":"Cholesterol","clicked":false,"image":""},{"title":"Covid","clicked":false,"image":""},{"title":"Diabetes","clicked":false,"image":""},{"title":"Doctor Speak","clicked":false,"image":""},{"title":"ENT","clicked":false,"image":""},{"title":"Eye Health","clicked":false,"image":""},{"title":"General Health","clicked":false,"image":""},{"title":"Health Tests","clicked":false,"image":""},{"title":"Health Heart","clicked":false,"image":""},{"title":"Homeopathy","clicked":false,"image":""},{"title":"Hypertension","clicked":false,"image":""},{"title":"Immunity","clicked":false,"image":""},{"title":"Mental Wellness","clicked":false,"image":""},{"title":"Nutrition","clicked":false,"image":""},{"title":"Oral Care","clicked":false,"image":""},{"title":"Women's Health","clicked":false,"image":""},{"title":"Skin & Hair","clicked":false,"image":""},{"title":"Thyroid","clicked":false,"image":""},{"title":"Yoga & Exercise","clicked":false,"image":""}]
+  int totalSelected = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(
-                'Select upto 5 health related categories',
-                style: GoogleFonts.dmSans(
-                  fontSize: 23,
-                  fontWeight: FontWeight.w500,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Text(
+                  'Select upto 5 health related categories',
+                  style: GoogleFonts.dmSans(
+                    color: Colors.black,
+                    fontSize: 23,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              GridView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2, crossAxisSpacing: 15),
-                  itemBuilder: (context, index) {
-                    return Container(
-                      child: Column(
-                        children: [
-                          Stack(
-                            children: [
-                              CircleAvatar(
-                                radius: 30,
-                                backgroundColor: Colors.blue,
+                SizedBox(
+                  height: 25,
+                ),
+                GridView.builder(
+                    itemCount: categoriesList.length,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2, crossAxisSpacing: 15),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.symmetric(horizontal: 15),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  if (categoriesList[index].clicked == false) {
+                                    if (totalSelected < 5) {
+                                      categoriesList[index].clicked = true;
+                                      totalSelected++;
+                                    } else {
+                                      Fluttertoast.showToast(
+                                          msg: 'Cannot Select more than 5');
+                                    }
+                                  } else if (categoriesList[index].clicked ==
+                                      true) {
+                                    categoriesList[index].clicked = false;
+                                    totalSelected--;
+                                  }
+                                });
+                              },
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    width: 120,
+                                    height: 120,
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: CustomColors.appBarColor),
+                                  ),
+                                  categoriesList[index].clicked
+                                      ? Container(
+                                          width: 120,
+                                          height: 120,
+                                          child: Icon(
+                                            Icons.check,
+                                            size: 100,
+                                            color: Colors.white,
+                                          ),
+                                          decoration: BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Colors.black54),
+                                        )
+                                      : Container()
+                                ],
                               ),
-                              
-                            ],
-                          ),
-                          Text(healthArticleHeading[index])
-                        ],
-                      ),
-                    );
-                  })
-            ],
+                            ),
+                            Text(
+                              categoriesList[index].title,
+                              style: GoogleFonts.dmSans(fontSize: 17),
+                              textAlign: TextAlign.start,
+                            )
+                          ],
+                        ),
+                      );
+                    })
+              ],
+            ),
           ),
         ),
       ),
